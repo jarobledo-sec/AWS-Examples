e3a59f09b94213476a6fce59987e253772759866
Juan Antonio <juanrobledomenendez@gmail.com>
2025-08-05 14:31:43 +0000

    powershell scripting for s3 fixed

--- s3/powershell-scripts/s3.ps1
+++ s3/powershell-scripts/s3.ps1
@@ -1,13 +1,19 @@
+# AWS s3 module is imported
 Import-Module AWS.Tools.S3
 
+# https://docs.aws.amazon.com/powershell/v5/reference/
+# set region
 $region = "eu-west-3"
+Set-DefaultAWSRegion -Region $region
 
+# name of the bucket is requested
 $bucketName = Read-Host -Prompt 'Enter the S3 bucket name'
 
-
+# region and bucket name varibles are displayed
 Write-Host "AWS Region: $region"
 Write-Host "S3 Bucket: $bucketName"
 
+# create a function to verify the existence of a bucket with that name
 function BucketExists {
     $bucket = Get-S3Bucket -BucketName $bucketName -ErrorAction SilentlyContinue
     return $null -ne $bucket
@@ -15,16 +21,16 @@ function BucketExists {
 
 if (-not (BucketExists)){
     Write-Host "Bucket does not exists..."
-    New-S3Bucket -BucketName $bucketName -Region $region 
+    New-S3Bucket -BucketName $bucketName # -Region $region
 } else {
     Write-Host "Bucket already exists..."
 } 
 
 # create a new file
-
 $fileName = 'myfile.txt'
 $fileContent = 'Hello World!'
 Set-Content -Path $fileName -Value $fileContent
 
-Write-S3Object -BucketName $bucketName -File $fileName -Key $fileName
+# upload objects
+Write-S3Object -BucketName $bucketName -File $fileName -Key $fileName # -Region $region
 
